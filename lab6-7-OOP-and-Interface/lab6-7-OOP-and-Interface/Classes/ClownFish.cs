b9679a5e4de5f966df159c6e1b90125182fc3777
Jin Kim <jinwoov@gmail.com>
2020-03-25 18:55:12 -0700

    putting a summar for all of the methods

--- lab6-7-OOP-and-Interface/lab6-7-OOP-and-Interface/Classes/ClownFish.cs
+++ lab6-7-OOP-and-Interface/lab6-7-OOP-and-Interface/Classes/ClownFish.cs
@@ -6,6 +6,9 @@ namespace lab6_7_OOP_and_Interface.Classes
 {
     public class ClownFish : Marine
     {
+        /// <summary>
+        /// Swimming methods from Fish and returning new string value.
+        /// </summary>
         public override string Swimming()
         {
             return "Stop swimming dory!";
--- lab6-7-OOP-and-Interface/lab6-7-OOP-and-Interface/Classes/Freshwater.cs
+++ lab6-7-OOP-and-Interface/lab6-7-OOP-and-Interface/Classes/Freshwater.cs
@@ -10,6 +10,9 @@ namespace lab6_7_OOP_and_Interface.Classes
 
         public sealed override string Food { get; set; } = "Algae";
 
+        /// <summary>
+        /// Method that will be passed on any `FreshWater` child. However will be overriden again due to virtual tag
+        /// </summary>
         public virtual bool CatchRelease()
         {
             return false;
